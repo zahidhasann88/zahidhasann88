cbbf6e77a58cbc6d4831ce1f7fc5cd6e540937c9
Zahid Hasan <jahidhasann67@gmail.com>
2024-09-18 13:11:45 +0600

    Refactor header and footer components, update navigation styling

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,21 +5,25 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule, ],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   navItems = [
     { path: '/', label: 'Home' },
     { path: '/projects', label: 'Projects' },
     { path: '/blog', label: 'Blog' },
-    { path: '/photos', label: 'Photos' }
+    { path: '/photos', label: 'Photos' },
   ];
 
   isMenuOpen = false;
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
   }
 }
