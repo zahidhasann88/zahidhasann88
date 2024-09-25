2e33f9e2ef7472a960ed9371c0b6f862b253bb7f
Zahid Hasan <jahidhasann67@gmail.com>
2024-09-25 19:21:58 +0600

    updated bio

--- src/app/data/static-data.ts
+++ src/app/data/static-data.ts
@@ -4,9 +4,8 @@ export const GLOBAL_STATE: Readonly<GlobalState> = {
   cvLink: 'https://drive.google.com/file/d/1_tNTjidYuIascSxhHtlQpDjbxrWfRZN5/view?usp=sharing',
   aboutMe: {
     part1: `My name is Zahid Hasan, also known as Zahid. I'm a 28-year-old Software Engineer from Dhaka, Bangladesh.`,
-    part2: `Currently, I work as a Software Engineer, focusing on backend development, with a keen interest in cloud technologies. 
-            I am skilled in cloud infrastructure provisioning, monitoring and serverless architectures. I enjoy seeking out creative solutions to 
-            complex problems and building things that empower others to do the same.`,
+    part2: `Currently, my focus is on web applications and infrastructure, with a strong interest in cloud technologies and distributed systems. 
+            I’m passionate about finding creative solutions to complex challenges and building tools that empower others to achieve more.`,
     part3: `Welcome to my own little corner of the web, where I share <a [routerLink]="['/blog']" class="inline-link">notes</a> and resources on topics that interest me. 
             I spend my free time building new <a [routerLink]="['/projects']" class="inline-link">things</a> and capturing in time
             <a [routerLink]="['/photos']" class="inline-link">moments</a>. For a closer look at my career journey, 
