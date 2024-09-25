28c3f574a5006af3f9511cc8c16d818c6c2f8a93
Zahid Hasan <jahidhasann67@gmail.com>
2024-09-25 19:30:49 +0600

    content updated

--- src/app/data/static-data.ts
+++ src/app/data/static-data.ts
@@ -1,10 +1,10 @@
 import { GalleryImage, GlobalState, Project } from '../models/global-state.model';
 
 export const GLOBAL_STATE: Readonly<GlobalState> = {
-  cvLink: 'https://drive.google.com/file/d/1_tNTjidYuIascSxhHtlQpDjbxrWfRZN5/view?usp=sharing',
+  cvLink: 'https://drive.google.com/file/d/1mbhr8CXB8KrRrDGIBt11mLUlkcgGBYlS/view?usp=sharing',
   aboutMe: {
     part1: `My name is Zahid Hasan, also known as Zahid. I'm a 28-year-old Software Engineer from Dhaka, Bangladesh.`,
-    part2: `Currently, my focus is on web applications and infrastructure, with a strong interest in cloud technologies and distributed systems. 
+    part2: `Currently, my focus is on web applications and infrastructure, with a background interest in cloud technologies and distributed systems. 
             I’m passionate about finding creative solutions to complex challenges and building tools that empower others to achieve more.`,
     part3: `Welcome to my own little corner of the web, where I share <a [routerLink]="['/blog']" class="inline-link">notes</a> and resources on topics that interest me. 
             I spend my free time building new <a [routerLink]="['/projects']" class="inline-link">things</a> and capturing in time
