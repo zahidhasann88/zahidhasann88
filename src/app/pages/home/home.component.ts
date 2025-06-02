25233ea0d8ae2c293b8b946515713e139a828776
Zahid Hasan <jahidhasann67@gmail.com>
2025-06-02 20:42:53 +0600

    feat: Add "See More About Me" button and social links to home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,15 @@
-import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { pageLoadAnimation, routeAnimations } from '../../utils/animation/animations';
+import { faGithub, faLinkedin, faTwitter, faStackOverflow, faMedium } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faCode } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
+import {
+  pageLoadAnimation,
+  routeAnimations,
+} from '../../utils/animation/animations';
 import { GlobalState } from '../../utils/models/global-state.model';
 import { GlobalStateService } from '../../utils/services/global-state.service';
 import { Subject } from 'rxjs';
@@ -11,11 +18,7 @@ import { takeUntil } from 'rxjs/operators';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterModule,
-    FontAwesomeModule,
-  ],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   animations: [routeAnimations, pageLoadAnimation],
@@ -23,6 +26,41 @@ import { takeUntil } from 'rxjs/operators';
 export class HomeComponent implements OnInit, OnDestroy {
   state!: GlobalState;
   private unsubscribe$ = new Subject<void>();
+
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
+  faTwitter = faTwitter;
+  faStackOverflow = faStackOverflow;
+  faMedium = faMedium;
+  faEnvelope = faEnvelope;
+  faCode = faCode;
+  faArrowUpLong = faArrowRightLong;
+  currentYear: number = new Date().getFullYear();
+  socialLinks = [
+    { name: 'GitHub', url: 'https://github.com/zahidhasann88', icon: faGithub },
+    {
+      name: 'LinkedIn',
+      url: 'https://linkedin.com/in/zahidhasann',
+      icon: faLinkedin,
+    },
+    { name: 'Twitter', url: 'https://x.com/zahidhasann15', icon: faTwitter },
+    {
+      name: 'Stack Overflow',
+      url: 'https://stackoverflow.com/users/12301102/zahid-hasan',
+      icon: faStackOverflow,
+    },
+    {
+      name: 'Medium',
+      url: 'https://medium.com/@jahidhasann67',
+      icon: faMedium,
+    },
+    { name: 'Email', url: 'mailto:zahidhasann67@gmail.com', icon: faEnvelope },
+    {
+      name: 'Leetcode',
+      url: 'https://leetcode.com/u/zahidhasann67/',
+      icon: faCode,
+    },
+  ];
 
   constructor(private globalStateService: GlobalStateService) {}
 
