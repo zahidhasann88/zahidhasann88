5ff019908464c69e741c6ff0fe28fe7e3b1c7d03
Zahid Hasan <jahidhasann67@gmail.com>
2025-06-02 20:54:01 +0600

    refactor: Remove footer component and update header styles for improved layout and branding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { routeAnimations } from './utils/animation/animations';
 import { SmoothScrollService } from './utils/services/smoth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, RouterModule],
+  imports: [RouterOutlet, HeaderComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  animations: [routeAnimations]
+  animations: [routeAnimations],
 })
 export class AppComponent {
   constructor(private smoothScrollService: SmoothScrollService) {}
 
   prepareRoute(outlet: RouterOutlet) {
-    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+    return (
+      outlet &&
+      outlet.activatedRouteData &&
+      outlet.activatedRouteData['animation']
+    );
   }
 
   scrollToElement(elementId: string): void {
