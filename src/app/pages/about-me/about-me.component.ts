35bb58302f3301673614830cfeb1af4c41037fbb
Zahid Hasan <jahidhasann67@gmail.com>
2025-06-04 13:39:58 +0600

    feat: Revamp About Me page layout and content with enhanced styling and structure

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCalendarDays } from '@fortawesome/free-solid-svg-icons';
 import {
   listAnimations,
   routeAnimations,
@@ -9,12 +11,13 @@ import {
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss',
   animations: [routeAnimations, listAnimations],
 })
 export class AboutMeComponent implements OnInit {
+  faCalendar = faCalendarDays;
 
   constructor() {}
 
